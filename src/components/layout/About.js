6f55e221460d466ce9149a58a5b59d792846f9ff
IslaMurtazaev <islam.muratazaev@gmail.com>
2019-01-15 21:00:08 +0600

    Update about page

--- src/components/layout/About.js
+++ src/components/layout/About.js
@@ -4,26 +4,39 @@ const About = () => {
   return (
     <div className="row">
       <div className="col-md-10 mx-auto jumbotron">
-        <h1 className="primary-text text-center">About</h1>
+        <h1 className="primary-text text-center">Mini bank</h1>
+        <h6 className="secondary-text text-center">
+          Is a serverless application where you can create an account for your
+          loaners and keep track of their paying back.
+        </h6>
         <h6 className="secondary-text text-center mb-4">
-          Project of <strong>CS 203: Intro to Web Technologies</strong>.
-          Computer Science department, AIU.
+          The purpose of this project is purely educational.
         </h6>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac
-          efficitur elit. Vestibulum libero est, facilisis at nisl vitae,
-          condimentum dignissim mi. Maecenas sed enim justo. Pellentesque
-          malesuada feugiat sapien non gravida. Pellentesque convallis lectus
-          risus, dictum mattis velit tempor a. Maecenas sodales sagittis tortor
-          at varius. Nulla venenatis risus tortor, vitae congue ipsum fermentum
-          ut. Sed vel nunc quis elit commodo convallis sit amet vitae est. Orci
-          varius natoque penatibus et magnis dis parturient montes, nascetur
-          ridiculus mus. Etiam tristique venenatis quam, vel porttitor urna
-          blandit vitae. Duis pulvinar quam in auctor malesuada. Etiam a risus
-          rhoncus, ultricies lorem sit amet, tincidunt libero. Vivamus et
-          aliquet nunc. Aenean pellentesque porta sapien nec feugiat.
+          We all go to some activities with out friends and occasionaly we pay
+          for each other and have hard time figuring out who owes whom and how
+          much. There are also situations when you can not remember when have
+          you managed to spend all your salary. Sometimes we keep track of our
+          spendings, by writing them down in notebook and eventually forgetting
+          about it for a month or so.
         </p>
-        <p>Stack of technologies: React, Bootstrap, Firebase, Firestore.</p>
+        <p>
+          With this app you can easily create an account for your loaner, with
+          his/her general infomation, contacts and balance. After you can edit
+          accounts and delete them if neccessary, also you can invite others to
+          signup and help you to keep track of the loaners if you are using this
+          app for a group of people (company, family).
+        </p>
+        <p>
+          This project is open-source, follow this link to
+          <a href="https://github.com/IslaMurtazaev/mini-bank">
+            my github repo
+          </a>
+          , you will find instructions for setup and launch.
+        </p>
+        <p>
+          Stack of technologies: React, Redux, Bootstrap, Firebase, Firestore.
+        </p>
       </div>
     </div>
   );
